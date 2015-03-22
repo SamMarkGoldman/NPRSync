78482a508623fd6b885a89989ec55aeda01922ee
Sam Goldman <sam@samgoldman.net>
2015-03-21 20:44:44 -0400

    fixes a silly big with tabs versus spaces.

--- NPRSyncScriptAndroidNoWifi.py
+++ NPRSyncScriptAndroidNoWifi.py
@@ -1,4 +1,3 @@
-import android
 import urllib2
 import os
 import datetime
@@ -7,12 +6,17 @@ from os.path import expanduser
 from bs4 import BeautifulSoup
 from ID3 import *
 
+from config import Config
 
+config = Config()
+
+if config.runOnAndroid:
+	import android
 
 
 print(os.getcwd())
 
-mp3Dir = expanduser('/storage/emulated/0/Media/NPR')
+mp3Dir = expanduser(config.mp3DirPath)
 serverConnectionErrorCount = 0
 #storyNum = 1
 
@@ -32,61 +36,64 @@ def getStoryLinks():
 	return links
 
 def deleteMp3s():
-    print("Deleteing previous files.")
-    filelist = [ f for f in os.listdir(mp3Dir) if f.endswith(".mp3") ]
-    for f in filelist:
-        os.remove(mp3Dir + '/' + f)
+	print("Deleteing previous files.")
+	filelist = [ f for f in os.listdir(mp3Dir) if f.endswith(".mp3") ]
+	for f in filelist:
+		os.remove(mp3Dir + '/' + f)
 
-    return
+	return
 
 def downloadMp3s( num ):
-    url = storyLinks[num]
-    global serverConnectionErrorCount
-    global storyNum
+	url = storyLinks[num]
+	global serverConnectionErrorCount
+	global storyNum
 
-    try:
-        stringNum = str(storyNum)
-    	if num < 10:
-    		stringNum = '0' + str(num)
+	try:
+		stringNum = str(storyNum)
+		print "before if"
+		if num < 10:
+			stringNum = '0' + str(num)
 
+		print "after if"
 		fileNamePieces = url.split("/")
 		fileNameFull = fileNamePieces[len(fileNamePieces) - 1]
 		fileName = fileNameFull.split("?")[0]
 		filePath = mp3Dir + '/' + fileName
+		print fileName
 		if os.path.isfile(filePath):
 			return
+
 		print("Attemping to download file at {0}".format(url))
 		response = urllib2.urlopen(url)
 		html = response.read()
+		if num == 1:
+			deleteMp3s()
 
-        if num == 1:
-            deleteMp3s()
-        file = open(filePath, 'w')
-        file.write(html)
-        file.close()
-
-        id3info = ID3(filePath)
-        oldTitle = id3info['TITLE']
-        id3info['TITLE'] = stringNum + oldTitle
-    except urllib2.HTTPError, e:
-        serverConnectionErrorCount += 1
-        print("Can't connect, {0} error".format(e.code))
-        time.sleep(600)
-        storyNum -= 1
-    except urllib2.URLError, e:
-        serverConnectionErrorCount += 1
-        print(e.args)
-        time.sleep(600)
-        storyNum -= 1
-    return
+		file = open(filePath, 'w')
+		file.write(html)
+		file.close()
+		id3info = ID3(filePath)
+		oldTitle = id3info['TITLE']
+		id3info['TITLE'] = stringNum + oldTitle
+	except urllib2.HTTPError, e:
+		serverConnectionErrorCount += 1
+		print("Can't connect, {0} error".format(e.code))
+		time.sleep(600)
+		storyNum -= 1
+	except urllib2.URLError, e:
+		serverConnectionErrorCount += 1
+		print(e.args)
+		time.sleep(600)
+		storyNum -= 1
+	return
 
 
 storyLinks = getStoryLinks()
 
 for storyNum in range(1, len(storyLinks)):
-    downloadMp3s(storyNum)
+	downloadMp3s(storyNum)
 
 
 #while serverConnectionErrorCount <= 3:
-#    downloadMp3s(storyNum)
-#    storyNum += 1
+#	downloadMp3s(storyNum)
+#	storyNum += 1
---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+	#  this is the directory that mp3 files will be stored after download
+	mp3DirPath = '/Users/Sam/CodeProjects/NPR Sync Project/testAudio'
+
+	#  help enable or disable the SL4A library includes.  This needs to be True if WiFi vs mobile data control is to be used.
+	runOnAndroid = False
